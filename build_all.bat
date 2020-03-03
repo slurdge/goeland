c10826ad8ee3240c31fc51175e903093b948ce72
slurdge <slurdge@slurdge.org>
2020-03-03 16:21:32 +0100

    better build all command for windows

--- build_all.bat
+++ build_all.bat
@@ -1,1 +1,3 @@
-gox -output="build/{{.Dir}}_{{.OS}}_{{.Arch}}" -ldflags "-X github.com/slurdge/goeland/version.GitCommit=${GIT_COMMIT}${GIT_DIRTY} -X github.com/slurdge/goeland/version.BuildDate=${BUILD_DATE}"
+FOR /F "tokens=* USEBACKQ" %%F IN (`git rev-parse HEAD`) DO (SET GIT_COMMIT=%%F)
+FOR /F "tokens=* USEBACKQ" %%F IN (`powershell get-date -format "{yyyyMMddHHmmss}"`) DO (SET BUILD_DATE=%%F)
+gox.py -parallel 4 -output "build/{{.Dir}}_{{.OS}}_{{.Arch}}" -ldflags "-X github.com/slurdge/goeland/version.GitCommit=%GIT_COMMIT% -X github.com/slurdge/goeland/version.BuildDate=%BUILD_DATE% -s -w"

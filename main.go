623a5443a477f6576da7735602fe6ccc3a2f2854
slurdge <slurdge@slurdge.org>
2020-05-23 11:18:04 +0200

    Support for go generate

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"github.com/slurdge/goeland/cmd"
 )
 
+//go:generate go-bindata -prefix "asset/" -pkg cmd -o cmd/bindata.go asset/
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	cmd.Execute()

62741165e57a95adf04b1890f215a98ef8b6cb48
slurdge <slurdge@slurdge.org>
2022-05-19 22:13:12 +0200

    fix: fix reddit rss #14

--- internal/goeland/httpget/httpget.go
+++ internal/goeland/httpget/httpget.go
@@ -2,6 +2,7 @@ package httpget
 
 import (
 	"crypto/tls"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 
@@ -9,24 +10,37 @@ import (
 )
 
 var UserAgent string = "multiple:goeland:" + version.Version + " (commit id:" + version.GitCommit + ") (by /u/goelandrss)"
+var defaultClient http.Client
 
 func GetHTTPRessource(url string) (body []byte, err error) {
 	request, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
 	}
-	//this one is needed because of incompatibility between latest golang and reddit
 	request.Header.Set("User-Agent", UserAgent)
 	request.Header.Set("Accept", "*/*")
-	var defaultClient = http.Client{
-		Transport: &http.Transport{
-			TLSNextProto: map[string]func(authority string, c *tls.Conn) http.RoundTripper{},
-		},
-	}
+
 	resp, err := defaultClient.Do(request)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
-	return ioutil.ReadAll(resp.Body)
+	body, err = ioutil.ReadAll(resp.Body)
+	if resp.StatusCode != 200 {
+		return body, fmt.Errorf("received error code %d", resp.StatusCode)
+	}
+	return body, err
+}
+
+func init() {
+	//this one is needed because of incompatibility between latest golang and reddit
+	defaultClient = http.Client{
+		Transport: &http.Transport{
+			//TLSNextProto: map[string]func(authority string, c *tls.Conn) http.RoundTripper{},
+			TLSClientConfig: &tls.Config{
+				//KeyLogWriter: dmp,
+			},
+			//ForceAttemptHTTP2: false,
+		},
+	}
 }

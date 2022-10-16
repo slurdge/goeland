9e4d3e1b49823361e4322a6b6274f977f4b3de53
slurdge <slurdge@slurdge.org>
2022-10-16 19:03:48 +0200

    feat: add basic i18n

---
+++ internal/goeland/i18n/messages.go
@@ -0,0 +1,12 @@
+package i18n
+
+import (
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
+)
+
+func init() {
+	message.SetString(language.BritishEnglish, "Digest for %s", "Digest for %s _uk")
+	message.SetString(language.AmericanEnglish, "Digest for %s", "Digest for %s _us")
+	message.SetString(language.French, "Digest for %s", "Abrégé de %s")
+}

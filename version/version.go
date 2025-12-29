d82eee76ce28888bdc5f4a690fdef3038f4c1e64
slurdge <slurdge@slurdge.org>
2025-12-29 21:08:52 +0100

    chore: nicer format

--- version/version.go
+++ version/version.go
@@ -16,9 +16,11 @@ var ChangeLog string
 
 // Version returns the main version number that is being run at the moment.
 // Given a version number MAJOR.MINOR.PATCH, increment the:
+//
 //     MAJOR version when you make incompatible API changes,
 //     MINOR version when you add functionality in a backwards compatible manner, and
 //     PATCH version when you make backwards compatible bug fixes.
+//
 // Additional labels for pre-release and build metadata are available as extensions to the MAJOR.MINOR.PATCH format.
 var Version = "dev"
 
